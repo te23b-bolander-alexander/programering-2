38933503ade70e5bc50b34f3285d0e6acf54f2c3
Blexander Bolander <alexander.bolander@elev.ga.ntig.se>
2025-09-05 12:22:10 +0200

    first commit

---
+++ highscore/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Drawing;
+
+List<string> names = ["Alex", "Sara"];
+List<int> points = [];
+names.Add("Swedlund");
+bool poängmednummer = false; 
+Console.WriteLine("Skriv namn");
+string efternamn = Console.ReadLine();
+int point = 0;
+names.Add(efternamn);
+while (poängmednummer == false)
+{
+    Console.WriteLine("skriv hurm ånga poäng den här personen förtjänar");
+    string poäng = Console.ReadLine();
+    poängmednummer = int.TryParse(poäng, out point);
+    if (poängmednummer == false) Console.WriteLine("skriv i nummer din goofball");
+
+}
+
+
+//stygg ike(jo(nope(beta(*sigma(*host*beta cuc *host* (*simga enigma med en stor mängd smegma som får alla hussar (nilo tengman ) och är tuff (no(jo)))))))) sigma
+
+
+Console.ReadLine();
