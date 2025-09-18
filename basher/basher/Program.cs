e2adb8948be63c1195af76e2c0ed0cdd33bfad09
Blexander Bolander <alexander.bolander@elev.ga.ntig.se>
2025-09-18 12:49:45 +0200

    abcdefghijklmnopqrstguvwxyzåäö säg det backlänges, Nej.

---
+++ basher/basher/Program.cs
@@ -0,0 +1,21 @@
+﻿Enemy jack = new();
+
+jack.Name = "jack";
+jack.Hp = 5;
+jack.Damage = 1;
+
+Enemy pegasus = new()
+{
+    Name = "pegasus",
+    Hp = 10,
+    Damage = 3
+};
+
+
+
+jack.Hp++;
+
+jack.Hp -= pegasus.Damage;
+
+
+
---
+++ basher/basher/enemy.cs
@@ -0,0 +1,7 @@
+public class Enemy
+{
+    public int Hp;
+    public int Damage;
+    public float X;
+    public string Name;
+}
