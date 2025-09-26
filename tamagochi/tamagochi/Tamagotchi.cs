9debdb1965dfcc73c0681f5c06d5b7c1d5cc1c22
Blexander Bolander <alexander.bolander@elev.ga.ntig.se>
2025-09-26 12:48:35 +0200

    gå tidigare

--- tamagochi/tamagochi/Tamagotchi.cs
+++ tamagochi/tamagochi/Tamagotchi.cs
@@ -1,36 +1,51 @@
+using System.ComponentModel;
 using System.Runtime.CompilerServices;
 
 public class Tamagotchi
 {
     private int hunger = 0;
     private int boredom = 0;
-    private List<string> words; 
-    private bool I_sAlive;
+    private List<string> words = ["Hallo"];
+    private bool IsAlive;
     private string name;
 
     public void Feed()
     {
-
+        hunger = -2;
+        if (hunger < 0)
+        {
+            hunger = 0;
+        }
     }
 
     public void Hi()
     {
-
+        Console.WriteLine($"{words[Random.Shared.Next(words.Count)]}");
+        ReduceBoredom();
     }
 
-    public void Teach()
+    public void Teach(String word)
     {
-        
+        words.Add(word);
     }
 
-    public void Tick() 
+    public void Tick()
     {
-
+        hunger = +1;
+        boredom = +1;
+        if (hunger > 9 || boredom > 9)
+        {
+            IsAlive = false;
+        }
     }
 
     private void ReduceBoredom()
     {
-
+        boredom = -2;
+        if (boredom < 0)
+        {
+            boredom = 0;
+        }
     }
     public void PrintStats()
     {
@@ -38,7 +53,7 @@ public class Tamagotchi
 
     }
 
-    public bool IsAlive()
+    public bool GetAlive()
     {
         return true;
     }
